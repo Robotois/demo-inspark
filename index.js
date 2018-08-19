a775ad51e50ea4191f30fcc7108c7407fbf1d877
manduks <iam@armando.mx>
2018-08-18 22:50:45 -0500

    added rgb

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Led = require("robotois-led");
+const RGBs = require("robotois-rgb-leds");
+const led = new Led(6);
+const rgb = new RGBs(2);
 
-const led = new Led(1);
-
-led.turnOn();
+// led
+led.blink(true);
 setTimeout(() => {
   led.turnOff();
-}, 2000);
+}, 6000);
+
+//rgb
+rgb.blink(1, `#FF0000`);
+rgb.blink(2, `#00FF00`);
+rgb.blink(3, `#0000FF`);
+rgb.blink(4, `#0F0F0F`);
+
+setTimeout(() => {
+  rgb.release();
+}, 8000);

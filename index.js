d21cf42bebaec8fc79ffd9e79f2f8bbd8acef4d2
manduks <iam@armando.mx>
2018-08-19 21:24:44 -0500

    se agrego el sensor de distancia

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 const Led = require("robotois-led");
 const RGBs = require("robotois-rgb-leds");
-const led = new Led(6);
-const rgb = new RGBs(2);
+const DSensor = require("robotois-distance-sensor");
+const sleep = require("sleep");
 
-// led
-led.blink(true);
-setTimeout(() => {
-  led.turnOff();
-}, 6000);
+const led = new Led(1);
+const rgb = new RGBs();
+const distance = new DSensor(2);
 
-//rgb
-rgb.blink(1, `#FF0000`);
-rgb.blink(2, `#00FF00`);
-rgb.blink(3, `#0000FF`);
-rgb.blink(4, `#0F0F0F`);
+let contador = 7;
+let intervalo;
+let intervalo2;
 
-setTimeout(() => {
-  rgb.release();
-}, 8000);
+function parpadear(numero) {
+  for (let i = 0; i < 4; i++) {
+    rgb.turnOn(numero, "#FFF00");
+    led.turnOn();
+    sleep.msleep(60);
+    rgb.turnOff(numero);
+    led.turnOff();
+    sleep.msleep(60);
+  }
+}
+
+function inicilizar() {
+  intervalo = setInterval(() => {
+    if (contador == 7) {
+      parpadear(contador);
+      contador--;
+    } else if (contador < 7 && contador > 0) {
+      rgb.turnOn(contador, "#0000FF");
+      if (contador == 4) {
+        rgb.turnOff(7);
+      }
+      contador--;
+    } else {
+      rgb.allOff();
+      contador = 7;
+    }
+  }, 100);
+}
+
+function festejo() {
+  intervalo2 = setInterval(() => {
+    const dist = Number(distance.getValue());
+    console.log(dist);
+
+    switch (dist) {
+      case dist < 10:
+        clearInterval(intervalo);
+        rgb.allOff();
+        led.turnOff();
+        rgb.blinkAll("#00FF00");
+        break;
+      default:
+        clearInterval(intervalo2);
+        inicilizar();
+        break;
+    }
+  }, 1000);
+}
+
+inicilizar();
+festejo();
